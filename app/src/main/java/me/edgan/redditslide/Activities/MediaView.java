09bca94e12c6f9d120fb6dde2ebbf7fc574b2505
PrimaryVision <155949587+PrimaryVision@users.noreply.github.com>
2025-05-17 22:09:49 +0530

    Run Spotless to auto-format code

--- app/src/main/java/me/edgan/redditslide/Activities/MediaView.java
+++ app/src/main/java/me/edgan/redditslide/Activities/MediaView.java
@@ -18,7 +18,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
