bb5da480d7cbaeeb5e861e2effd59c045cb7ac1a
Nathan Grennan <github@cygnusx-1.org>
2025-05-20 13:32:55 -0700

    Fixed WindowManager in MediaView.java

--- app/src/main/java/me/edgan/redditslide/Activities/MediaView.java
+++ app/src/main/java/me/edgan/redditslide/Activities/MediaView.java
@@ -19,6 +19,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
